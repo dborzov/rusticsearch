b4c537725138582d39c9e72a342bd5407350d1a5
Peter Borzov <peter@sowingo.com>
2014-05-02 15:41:27 -0400

    some logging

--- handlers.go
+++ handlers.go
@@ -4,23 +4,43 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
+	"time"
 )
 
 type Handler func(w http.ResponseWriter, r *http.Request)
+type Filler func(r *http.Request) []byte
 
-func papaHandler(a Handler) Handler {
+func papaHandler(a Filler) Handler {
 	rh := func(w http.ResponseWriter, r *http.Request) {
+		//setting the desired header
 		h := w.Header()
 		h.Set("Access-Control-Allow-Origin", "*")
 		h.Set("Access-Control-Allow-Methods", "POST, GET, PUT, PATCH, DELETE, OPTIONS")
 		h.Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, API-Date, Host, Authorization, Key, User-Token")
 		h.Set("Access-Control-Max-Age", "1728000")
-		a(w, r)
+		response := string(a(r))
+		fmt.Fprintf(w, response)
+		// now log the fucker
+		file, err := os.OpenFile("history.log", os.O_RDWR|os.O_APPEND, 0660)
+		defer file.Close()
+		if err != nil {
+			fmt.Printf("Having trouble with Logger file here \n")
+			return
+		}
+		file.WriteString("\n\n---------------------------\n")
+		file.WriteString(fmt.Sprintf("Time is %s:\n", time.Now()))
+		file.WriteString("Yet another request:\n\n")
+
+		jr, _ := json.MarshalIndent(*r, "    ", "   ")
+		file.Write(jr)
+		file.WriteString("\n\nOur response shall be:\n\n")
+		file.WriteString(response)
 	}
 	return rh
 }
 
-func handler_autocomplete(w http.ResponseWriter, r *http.Request) {
+func handler_autocomplete(r *http.Request) []byte {
 	search_query = r.URL.Path[14:]
 	fmt.Printf("AUTOCOMPLETE REQUEST: %s \n", search_query)
 	results, _ := SearchEngine.Query(search_query, 5)
@@ -34,16 +54,15 @@ func handler_autocomplete(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Printf("Error with jsonifying %s \n ", err)
 	}
-
-	fmt.Fprintf(w, string(searchResults))
+	return searchResults
 }
 
-func handler_welcome(w http.ResponseWriter, r *http.Request) {
+func handler_welcome(r *http.Request) []byte {
 	fmt.Printf("HELLO REQUEST:\n")
-	fmt.Fprintf(w, "Welcome to Rustic Search!")
+	return []byte("Welcome to Rustic Search!")
 }
 
-func handler_searchpage(w http.ResponseWriter, r *http.Request) {
+func handler_searchpage(r *http.Request) []byte {
 	search_query = r.URL.Path[12:]
 	fmt.Printf("SEARCHPAGE REQUEST: %s \n", search_query)
 	results, _ := SearchEngine.Query(search_query, 100)
@@ -57,5 +76,5 @@ func handler_searchpage(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Fprintf(w, string(searchResults))
+	return searchResults
 }

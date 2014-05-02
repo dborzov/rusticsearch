cd0066a7d42afb8f875af008f41ca18ca8aad0fa
Peter Borzov <peter@sowingo.com>
2014-05-02 17:38:59 -0400

    header hardcoded

--- server.go
+++ server.go
@@ -9,6 +9,13 @@ type Server struct {
 	endpoints []Endpoint
 }
 
+var Header = http.Header{
+	"Access-Control-Allow-Origin":  []string{"*"},
+	"Access-Control-Allow-Methods": []string{"POST, GET, PUT, PATCH, DELETE, OPTIONS"},
+	"Access-Control-Allow-Headers": []string{"Origin, X-Requested-With, Content-Type, Accept, API-Date, Host, Authorization, Key, User-Token"},
+	"Access-Control-Max-Age":       []string{"1728000"},
+}
+
 type Filler func(r *http.Request) []byte
 
 type Endpoint struct {
@@ -17,17 +24,21 @@ type Endpoint struct {
 }
 
 func (this Endpoint) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
+	h := w.Header()
 }
 
 func SetItUp() {
-	searchpage := Endpoint{
-		path:    "/searchpage/",
-		reactor: handler_searchpage,
-	}
+	// searchpage := Endpoint{
+	// 	path:    "/searchpage/",
+	// 	reactor: handler_searchpage,
+	// }
 
-	autocomplete := Endpoint{
-		path:    "/autocomplete/",
-		reactor: handler_autocomplete,
-	}
+	// autocomplete := Endpoint{
+	// 	path:    "/autocomplete/",
+	// 	reactor: handler_autocomplete,
+	// }
+
+	//    return Server{
+	//        headers
+	//    }
 }

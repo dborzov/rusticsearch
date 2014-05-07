ee609e57a9ff4e2645272c301db56ba3dfef0eb3
Peter Borzov <peter@sowingo.com>
2014-05-07 17:08:56 -0400

    Searching by single words

--- searchresults.go
+++ searchresults.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+)
+
 type SearchItem struct {
 	Id   string `json:"id"`
 	Name string `json:"name"`
@@ -28,11 +33,21 @@ func SearchQuery(query string, count int) []interface{} {
 	// ideally, the result would exactly match
 	results, _ := SearchEngine.Query(query, count)
 	output := make([]interface{}, len(results))
-	for i, word := range results {
-		output[i] = ValueIds[string(word)]
+	for i, item := range results {
+		output[i] = ValueIds[string(item)]
 	}
 	if len(results) != 0 {
 		return output
+	}
+
+	// exact match did not work, try word by word
+	words := bytes.Split([]byte(query), []byte(" "))
+	fmt.Printf("The words: ", words, " \n")
+	for _, word := range words {
+		results, _ := SearchEngine.Query(string(word), count)
+		for _, item := range results {
+			output = append(output, ValueIds[string(item)])
+		}
 	}
 
 	return output
